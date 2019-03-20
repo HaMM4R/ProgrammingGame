7fd163733be3b94bf492bee2811aaa2cd7f2ec6d
HaMM4R <HaMM4RTime@hotmail.com>
2019-03-20 14:38:31 +0000

    Camera following tank

---
+++ Assets/Scripts/Player/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    GameObject cam;
+
+    void Start()
+    {
+        GetCamera(); 
+    }
+
+    void GetCamera()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
+    }
+
+}
